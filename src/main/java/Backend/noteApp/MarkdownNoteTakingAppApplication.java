6def768922fbd7a799ad3727fd43a1d747b2a606
MohameD-SaiD <muhamdsaid253@gmail.com>
2024-11-10 15:04:57 +0200

    Environment setup and database connection

---
+++ src/main/java/Backend/noteApp/MarkdownNoteTakingAppApplication.java
@@ -0,0 +1,13 @@
+package Backend.noteApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownNoteTakingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkdownNoteTakingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Backend/noteApp/MarkdownNoteTakingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend.noteApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownNoteTakingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
